0b2594b59fb3b6d9ba66119c0f1d284c88be178f
Neucrack <CZD666666@gmail.com>
2021-01-20 14:32:07 +0800

    add nn.forward

--- ext_modules/_maix_nn/src/py_maix_nn_Model.c
+++ ext_modules/_maix_nn/src/py_maix_nn_Model.c
@@ -1,7 +1,7 @@
 #include "py_maix_nn.h"
 #include <string.h>
 #include "libmaix_nn.h"
-
+#include "stdlib.h"
 
 
 
@@ -9,6 +9,10 @@ typedef struct
 {
     PyObject_HEAD;
     libmaix_nn_t* nn;
+    float*        quantize_buffer;
+    float*        out_buffer;
+    PyObject*     inputs;
+    PyObject*     outputs;
 } ModelObject;
 
 PyDoc_STRVAR(Maix_NN_Object_type_doc, "neural network model object.\n");
@@ -23,6 +27,10 @@ static PyObject* Model_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
         return NULL;
     }
     self->nn = NULL;
+    self->quantize_buffer = NULL;
+    self->out_buffer      = NULL;
+    self->inputs = NULL;
+    self->outputs = NULL;
     return (PyObject*)self;
 }
 
@@ -89,19 +97,21 @@ static int Model_init(ModelObject *self, PyObject *args, PyObject *kwds)
     if(strcmp(model_type, "awnn") == 0)
     {
         PyObject *o_inputs = PyDict_GetItemString(o_opt, "inputs");
-        if(!o_inputs || !PyList_Check(o_inputs))
+        if(!o_inputs || !PyDict_Check(o_inputs))
         {
-            PyErr_SetString(PyExc_ValueError, "arg opt need inputs key, value is list");
+            PyErr_SetString(PyExc_ValueError, "arg opt need inputs key, value is dict");
             return -1;
         }
-        Py_ssize_t inputs_len = PyList_Size(o_inputs);
-        PyObject *o_outputs = PyDict_GetItemString(o_opt, "inputs");
-        if(!o_outputs || !PyList_Check(o_outputs))
+        Py_ssize_t inputs_len = PyDict_Size(o_inputs);
+        self->inputs = o_inputs;
+        PyObject *o_outputs = PyDict_GetItemString(o_opt, "outputs");
+        if(!o_outputs || !PyDict_Check(o_outputs))
         {
-            PyErr_SetString(PyExc_ValueError, "arg opt need outputs key, value is list");
+            PyErr_SetString(PyExc_ValueError, "arg opt need outputs key, value is dict");
             return -1;
         }
-        Py_ssize_t outputs_len = PyList_Size(o_outputs);
+        Py_ssize_t outputs_len = PyDict_Size(o_outputs);
+        self->outputs = o_outputs;
         PyObject *o_fl_conv_no_pad = PyDict_GetItemString(o_opt, "first_layer_conv_no_pad");
         if(!o_fl_conv_no_pad || !PyBool_Check(o_fl_conv_no_pad))
         {
@@ -142,13 +152,15 @@ static int Model_init(ModelObject *self, PyObject *args, PyObject *kwds)
         };
         char* inputs_names[inputs_len];
         char* outputs_names[outputs_len];
+        PyObject* keys_inputs = PyDict_Keys(o_inputs);
+        PyObject* keys_outputs = PyDict_Keys(o_outputs);
         for(Py_ssize_t i=0; i<inputs_len; ++i)
         {
-            inputs_names[i] = (char*)PyUnicode_DATA(PyList_GetItem(o_inputs, i));
+            inputs_names[i] = (char*)PyUnicode_DATA(PyList_GetItem(keys_inputs, i));
         }
         for(Py_ssize_t i=0; i<outputs_len; ++i)
         {
-            outputs_names[i] = (char*)PyUnicode_DATA(PyList_GetItem(o_outputs, i));
+            outputs_names[i] = (char*)PyUnicode_DATA(PyList_GetItem(keys_outputs, i));
         }
         libmaix_nn_opt_param_t opt_param = {
             .awnn.input_names             = inputs_names,
@@ -179,6 +191,7 @@ static int Model_init(ModelObject *self, PyObject *args, PyObject *kwds)
             PyErr_Format(PyExc_Exception, "libmaix_nn init fail: %s\n", libmaix_get_err_msg(err));
             goto end;
         }
+        printf("-----load now----\n");
         err = self->nn->load(self->nn, &model_path, &opt_param);
         if(err != LIBMAIX_ERR_NONE)
         {
@@ -217,11 +230,11 @@ static PyObject *Model_str(PyObject *object)
 PyDoc_STRVAR(Model_forward_doc, "forward network.\n");
 static PyObject* Model_forward(ModelObject *self, PyObject *args, PyObject *kw_args)
 {
+    libmaix_err_t err = LIBMAIX_ERR_NONE;
     PyObject *o_inputs = NULL;
     int quantize = 0;
     Py_ssize_t buflen, readlen, recvlen = 0;
     static char *kwlist[] = {"inputs", "quantize", NULL};
-
     /* Get the buffer's memory */
     if (!PyArg_ParseTupleAndKeywords(args, kw_args, "O|$p:forward", kwlist,
                                      &o_inputs, &quantize))
@@ -239,12 +252,142 @@ static PyObject* Model_forward(ModelObject *self, PyObject *args, PyObject *kw_a
         PyErr_SetString(PyExc_NotImplementedError, "not support multiple input yet");
         return NULL;
     }
-    if(o_inputs->ob_type->tp_name);
-    // else if()
-    // {
-
-    // }
-    return PyUnicode_InternFromString("echo from forward");
+    //TODO: support numpy and bytes and list
+    if(strstr(o_inputs->ob_type->tp_name, "Image") < 0)
+    {
+        PyErr_SetString(PyExc_ValueError, "not supported input object");
+        return NULL;
+    }
+    PyObject* o_input_bytes = PyObject_CallMethod(o_inputs, "tobytes", NULL);
+    if(o_input_bytes == NULL)
+    {
+        return NULL;
+    }
+    char* input_bytes = PyBytes_AsString(o_input_bytes);
+    if(input_bytes == NULL)
+    {
+        PyErr_SetString(PyExc_ValueError, "get bytes data error");
+        return NULL;
+    }
+    PyObject* o_width = PyObject_GetAttrString(o_inputs, "width");
+    if(o_width == NULL)
+    {
+        return NULL;
+    }
+    long input_width = PyLong_AsLong(o_width);
+    PyObject* o_height = PyObject_GetAttrString(o_inputs, "height");
+    if(o_height == NULL)
+    {
+        return NULL;
+    }
+    long input_height = PyLong_AsLong(o_height);
+    PyObject* o_mode = PyObject_GetAttrString(o_inputs, "mode");
+    if(o_mode == NULL)
+    {
+        return NULL;
+    }
+    if(strcmp(PyUnicode_DATA(o_mode), "RGB") != 0)
+    {
+        PyErr_SetString(PyExc_NotImplementedError, "only support RGB mode picture");
+        return NULL;
+    }
+    long input_channel = 3;
+    PyObject* o_inputs_shape = PyDict_Values(self->inputs);
+    PyObject* o_input_shape = PyList_GetItem(o_inputs_shape, 0);
+    long input_w=0, input_h=0, input_c=0;
+    if(PyTuple_Check(o_input_shape))
+    {
+        input_w = PyLong_AsLong(PyTuple_GetItem(o_input_shape, 1));
+        input_h = PyLong_AsLong(PyTuple_GetItem(o_input_shape, 0));
+        input_c = PyLong_AsLong(PyTuple_GetItem(o_input_shape, 2));
+    }
+    else
+    {
+        input_w = PyLong_AsLong(PyList_GetItem(o_input_shape, 1));
+        input_h = PyLong_AsLong(PyList_GetItem(o_input_shape, 0));
+        input_c = PyLong_AsLong(PyList_GetItem(o_input_shape, 2));
+    }
+    if(input_w != input_width || input_h != input_height || input_c != input_channel)
+    {
+        PyErr_Format(PyExc_ValueError, "input shape error, need:(%d, %d, %d), but(%d, %d, %d)", input_h, input_w, input_c, input_height, input_width, input_channel);
+        return NULL;
+    }
+    // printf("input shape, need:(%d, %d, %d), actually (%d, %d, %d)\n", input_h, input_w, input_c, input_height, input_width, input_channel);
+    /* libmaix API */
+    libmaix_nn_layer_t input = {
+        .w = input_width,
+        .h = input_height,
+        .c = input_channel,
+        .dtype = (quantize == 0 ? LIBMAIX_NN_DTYPE_INT8 : LIBMAIX_NN_DTYPE_UINT8),
+        .data = input_bytes,
+        .need_quantization = (quantize == 0 ? false : true),
+        .buff_quantization = NULL
+    };
+    PyObject* o_outputs_shape = PyDict_Values(self->outputs);
+    PyObject* o_output_shape = PyList_GetItem(o_outputs_shape, 0);
+    long output_w=0, output_h=0, output_c=0;
+    if(PyTuple_Check(o_output_shape))
+    {
+        output_w = PyLong_AsLong(PyTuple_GetItem(o_output_shape, 1));
+        output_h = PyLong_AsLong(PyTuple_GetItem(o_output_shape, 0));
+        output_c = PyLong_AsLong(PyTuple_GetItem(o_output_shape, 2));
+    }
+    else
+    {
+        output_w = PyLong_AsLong(PyList_GetItem(o_output_shape, 1));
+        output_h = PyLong_AsLong(PyList_GetItem(o_output_shape, 0));
+        output_c = PyLong_AsLong(PyList_GetItem(o_output_shape, 2));
+    }
+    libmaix_nn_layer_t out_fmap = {
+        .w = output_w,
+        .h = output_h,
+        .c = output_c,
+        .dtype = LIBMAIX_NN_DTYPE_FLOAT,
+        .data = NULL
+    };
+    Py_ssize_t _size = input.w * input.h * input.c;
+    if(input.need_quantization)
+    {
+        if(!self->quantize_buffer)
+        {
+            uint8_t* quantize_buffer = (uint8_t*)malloc(_size);
+            if(!quantize_buffer)
+            {
+                PyErr_Format(PyExc_MemoryError, "no memory for quantize buffer, size:%d", _size);
+                return NULL;
+            }
+            self->quantize_buffer = quantize_buffer;
+        }
+        input.buff_quantization = self->quantize_buffer;
+    }
+    _size = out_fmap.w * out_fmap.h * out_fmap.c * sizeof(float);
+    if(!self->out_buffer)
+    {
+        self->out_buffer = (float*)malloc(_size);
+        if(!self->out_buffer)
+        {
+            PyErr_Format(PyExc_MemoryError, "no memory for out buffer, need size:%d", _size);
+            return NULL;
+        }
+    }
+    out_fmap.data = self->out_buffer;
+    err = self->nn->forward(self->nn, &input, &out_fmap);
+    if(err != LIBMAIX_ERR_NONE)
+    {
+        PyErr_Format(PyExc_Exception, "libmaix_nn forward fail: %s\n", libmaix_get_err_msg(err));
+        return NULL;
+    }
+    PyObject* result_bytes = PyBytes_FromStringAndSize((const char*)out_fmap.data, _size);
+    PyObject* numpy = PyImport_ImportModule("numpy");
+    if (numpy == NULL)
+        return NULL;
+    PyObject *call_args = Py_BuildValue("(O)", result_bytes);
+    PyObject *call_keywords = PyDict_New();
+    PyDict_SetItemString(call_keywords, "dtype", PyUnicode_FromString("float32"));
+    PyObject* o_result_numpy = PyObject_Call(PyObject_GetAttrString(numpy, "frombuffer"), call_args, call_keywords);
+    Py_DECREF(call_args);
+    Py_DECREF(call_keywords);
+    return o_result_numpy;
 }
 static PyMethodDef Model_methods[] = {
     {"forward", (PyCFunction)Model_forward, METH_VARARGS | METH_KEYWORDS, Model_forward_doc},

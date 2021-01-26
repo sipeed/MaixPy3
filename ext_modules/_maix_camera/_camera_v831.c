645ccd3dcd47fd6d9e36b024933753b21bf27a22
junhuanchen <junhuanchen@qq.com>
2021-01-26 13:13:05 +0800

    [camera] capture usleep(10 * 1000).

--- ext_modules/_maix_camera/_camera_v831.c
+++ ext_modules/_maix_camera/_camera_v831.c
@@ -139,7 +139,7 @@ static PyObject *V831Camera_read(V831CameraObject *self, PyObject *args)
       // not ready， sleep to release CPU
       if(ret == LIBMAIX_ERR_NOT_READY)
       {
-          usleep(20 * 1000);
+          usleep(10 * 1000);
           continue;
       }
       if(ret == LIBMAIX_ERR_NONE)

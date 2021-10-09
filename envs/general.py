dde289d566c1e1781acd2264f802ae84fefc937e
junhuanchen <junhuanchen@qq.com>
2021-10-09 14:13:53 +0800

    [clear]

--- envs/general.py
+++ envs/general.py
@@ -12,19 +12,9 @@ _maix_module = Extension('_maix', include_dirs=['ext_modules/_maix/include'],
 ],
 )
 
-_maix_camera_module = Extension('_maix_camera', include_dirs=['ext_modules/_maix_camera/include'],
-                                sources=get_srcs('ext_modules/_maix_camera'),
-                                )
-
-_maix_display_module = Extension('_maix_display', include_dirs=['ext_modules/_maix_display/include'],
-                                 sources=get_srcs('ext_modules/_maix_display'),
-                                 )
-
 _maix_modules = [
     libi2c_module,
-    _maix_module,
-    _maix_camera_module,
-    _maix_display_module
+    _maix_module
 ]
 
 _maix_data_files = [

3168ed8e7e8a823fda9823effce13519f2a45719
YanxingLiu <liuyanxing98@foxmail.com>
2022-04-10 15:38:12 +0800

    add lens_corr and mean filter

--- ext_modules/_maix_image/example/maix_image_lens_corr.py
+++ ext_modules/_maix_image/example/maix_image_lens_corr.py
@@ -1,7 +1,7 @@
 from maix import  display, image 
 import time
 tmp = image.Image()
-tmp.open("/home/lyx/datasets/test_data")
-tmp = tmp.gamma_corr()
-# display.show(tmp) 
-# time.sleep(10)
+tmp.open("/home/lyx/images/test_filter.jpg")
+tmp = tmp.lens_corr()
+display.show(tmp) 
+time.sleep(10)
--- ext_modules/_maix_image/example/maix_image_mean.py
+++ ext_modules/_maix_image/example/maix_image_mean.py
@@ -1,7 +1,7 @@
 from maix import  display, image 
 import time
 tmp = image.Image()
-tmp.open("/home/lyx/Desktop/src2.png")
-tmp = tmp.gamma_corr()
+tmp.open("/home/lyx/images/test_filter.jpg")
+tmp = tmp.mean(2)
 display.show(tmp) 
 time.sleep(10)

e1519614e5ca3a03d3a389846b01b3f90c4d4df0
junhuanchen <junhuanchen@qq.com>
2022-05-31 18:01:33 +0800

    [evdev] test touchscreen

--- tests/general/uasge_evdev.py
+++ tests/general/uasge_evdev.py
@@ -1,22 +1,31 @@
 from maix import event, display, image
 from select import select
-
+import os
+os.system("insmod /lib/modules/4.9.118/cst816.ko")
 dev = event.InputDevice("/dev/input/event1")
+img = display.as_image()
 while True:
+  display.show(img)
   r, w, x = select([dev], [], [], 0) # if r == 0 or set 0 will read() raise BlockingIOError
   if r:
-    img = display.as_image()
-    img.draw_rectangle(0, 0, 240, 240, color=(255, 0, 0), thickness=-1)
-    x, y = 0, 0
+    x, y, e = 0, 0, 0
     for data in dev.read():
       if (data.type == 0x03):
-        print(' type: ', data.type, ' code: ', hex(data.code), ' value: ', hex(data.value))
+        # print(' type: ', data.type, ' code: ', hex(data.code), ' value: ', hex(data.value))
         if data.code == 0x35: # ABS_MT_POSITION_X
           x = data.value
         if data.code == 0x36: # ABS_MT_POSITION_Y
           y = data.value
         if data.code == 0x30: # ABS_MT_TOUCH_MAJOR
-          tmp = "(%d, %d, %d)" % (x, y, hex(data.value))
+          e = data.value
+        if data.code == 0x32: # ABS_MT_WIDTH_MAJOR
+          if data.value == 0:
+            img.draw_rectangle(0, 0, 240, 240, color=(0, 0, 0), thickness=-1)
+          else:
+            img.draw_circle(x, y, 2, color=(255, 0, 0), thickness=-1)
+          tmp = "(%d, %d, %d, %s)" % (data.value, x, y, hex(e))
           w, h = image.get_string_size(tmp)
-          img.draw_string(120 - w // 2, 120, tmp)
-          display.show(img)
+          bak = image.new(size=(w + 20, h + 2))
+          bak.draw_string(0, 0, tmp)
+          img.draw_image(bak, 120 - w // 2, 120)
+

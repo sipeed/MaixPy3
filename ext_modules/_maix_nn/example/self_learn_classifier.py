2857ac1268c1f826cf50fa161d2d804f7b37e257
Neucrack <8625829+Neutree@users.noreply.github.com>
2022-08-22 19:26:35 +0800

    Update self_learn_classifier.py

--- ext_modules/_maix_nn/example/self_learn_classifier.py
+++ ext_modules/_maix_nn/example/self_learn_classifier.py
@@ -1,8 +1,32 @@
+
+#
+# self learning classifier
+# download model from https://maixhub.com/model/zoo/86 first
+#
+
 from maix import nn
 from PIL import Image, ImageDraw
 from maix import camera, display
 import time
 from maix.nn.app.classifier import Classifier, load
+from evdev import InputDevice
+from select import select
+
+def get_keys(device):
+    '''
+        @return is_left_pressed, is_right_pressed
+    '''
+    left = False
+    right = False
+    r,w,x = select([device], [], [], 0)
+    if r:
+        for event in device.read():
+            if event.value == 1 and event.code == 0x02:
+                right = True
+            elif event.value == 1 and event.code == 0x03:
+                left = True
+    return left, right
+
 
 class_num = 3
 sample_num = 15
@@ -10,40 +34,53 @@ feature_len = 512
 input_w = 224
 input_h = 224
 
-model = {
-    "param": "/root/models/resnet18_1000_awnn.param",
-    "bin": "/root/models/resnet18_1000_awnn.bin"
-}
-
-options = {
-    "model_type":  "awnn",
-    "inputs": {
-        "input0": (input_w, input_h, 3)
-    },
-    "outputs": {
-        "190": (1, 1, feature_len)
-    },
-    "mean": [127.5, 127.5, 127.5],
-    "norm": [0.0176, 0.0176, 0.0176],
-}
+model = "./resnet18_1000_awnn.mud"
 print("-- load model:", model)
-m = nn.load(model, opt=options)
+m = nn.load(model)
 print("-- load ok")
 
+# model = {
+#     "param": "./resnet18_1000_awnn.param",
+#     "bin": "./resnet18_1000_awnn.bin"
+# }
+
+# options = {
+#     "model_type":  "awnn",
+#     "inputs": {
+#         "input0": (input_w, input_h, 3)
+#     },
+#     "outputs": {
+#         "190": (1, 1, feature_len)
+#     },
+#     "mean": [127.5, 127.5, 127.5],
+#     "norm": [0.0176, 0.0176, 0.0176],
+# }
+# print("-- load model:", model)
+# m = nn.load(model, opt=options)
+# print("-- load ok")
+
+
+keys_device = InputDevice('/dev/input/event0')
 def key_pressed():
-    # TODO: is button pressed
-    return 1
+    l, r = get_keys(keys_device)
+    return l, r
+
+key_l = False
+key_r = False
 
 classifier = Classifier(m, class_num, sample_num, feature_len, input_w, input_h)
 
 curr_class = 0
 curr_sample = 0
 while 1:
-    img = camera.capture().resize(size=(224, 224))
+    img = camera.capture().resize(size=(input_w, input_h))
     if not img:
         time.sleep(0.02)
         continue
-    if key_pressed():
+    l, r = key_pressed()
+    key_l = l | key_l
+    key_r = r | key_r
+    if key_l:
         if curr_class < class_num:
             print("add class")
             classifier.add_class_img(img)
@@ -61,6 +98,14 @@ while 1:
                 print("test load")
                 classifier = load(m, "m.classifier")
                 print("test load ok")
-    idx, distance = classifier.predict(img)
-    print("predict class: {}, distance: {}".format(idx, distance))
+        key_l = False
+    if curr_class < class_num:
+        img.draw_string(0, 0, f"press to record class {curr_class}", color=(255, 0, 0))
+    elif curr_sample < sample_num:
+        img.draw_string(0, 0, f"press to record sample {curr_sample}", color=(255, 0, 0))
+    else:
+        idx, distance = classifier.predict(img)
+        img.draw_string(0, 0, f"class {idx}, distance: {distance}", color=(255, 0, 0))
+        print("predict class: {}, distance: {}".format(idx, distance))
+    display.show(img)
 
